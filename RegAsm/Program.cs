79e9a48035431205d3955ce5cc1074665cd93ff5
jirolabo <jirolabo@outlook.com>
2018-03-21 22:41:31 +0900

    初期作成

--- RegAsm/Program.cs
+++ RegAsm/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
 
-namespace RegAsm
+namespace RegAsmExecute
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var path = Path.Combine(RuntimeEnvironment.GetRuntimeDirectory(), "RegAsm.exe");
+            using (var process = new Process())
+            {
+                process.StartInfo.FileName = path;
+                process.StartInfo.Arguments = String.Join(" ", args);
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+
+                process.Start();
+                process.WaitForExit();
+                var output = process.StandardOutput.ReadToEnd();
+                var error = process.StandardError.ReadToEnd();
+                process.Close();
+
+                Console.Write(output);
+                Console.Write(error);
+            }
         }
     }
 }
